fd1a25f8809d906916fed1408d7c28a0135c0b17
Hans Doller <kryo2k@gmail.com>
2013-10-09 22:22:15 -0600

    Improved shutdown procedure

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,18 @@
 (function(app){
-    app.bootstrap({
-        // default config overrides here
-    }).startServer();
+    var
+    instance = app.bootstrap(),
+    log = app.getLog();
+
+    instance.startServer();
+
+    process.on('SIGINT', function() {
+        log.debug('Got SIGINT. Exiting process.');
+        process.exit();
+    });
+    process.on('exit', function(){
+        log.debug("Application exiting");
+        instance.shutdown();
+    });
 })(
     require("./lib/application.js")
 );
