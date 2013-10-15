fb3bbf07e3ce0d078c47ed0e920bd0cbc4849412
Hans Doller <kryo2k@gmail.com>
2013-10-15 00:38:57 -0600

    Improvements to server-side.

--- run.js
+++ run.js
@@ -4,6 +4,18 @@ global.config = {
 
 (function(core) {
     core.boot(function(instance){
-        core.get('webserver').start();
+        core.get('webserver').start(function(){
+        });
+    });
+    process.on('SIGINT', function() {
+        core.debug('Got SIGINT. Exiting process.');
+        process.exit();
+    });
+    process.on('exit', function(){
+        core.get('webserver').stop(function(){
+            core.shutdown(function(){
+                core.debug("good bye!");
+            });
+        });
     });
 })(require('./lib/core'));
